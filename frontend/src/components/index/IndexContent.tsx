f9c59818497718344b6fc19aac8a56fa0d6a1262
MRmlik12 <mrmlik12sup@gmail.com>
2022-04-01 10:36:46 +0100

    Add index view prototype

---
+++ frontend/src/components/index/IndexContent.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../../Styles/index/IndexContent.sass";
+
+const IndexContent = () => {
+  return (
+    <div id="index-content">
+      <button>Create Voting</button>
+      <button>Join Voting</button>
+    </div>
+  )
+}
+
+export default IndexContent;
