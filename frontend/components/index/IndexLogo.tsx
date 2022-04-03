5604d6f26b7179c734b225e4fc69088d7d5f754a
MRmlik12 <mrmlik12sup@gmail.com>
2022-04-03 20:02:50 +0200

    Add index prototype view

---
+++ frontend/components/index/IndexLogo.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const IndexLogo = () => {
+  return (
+    <div className="flex flex-row items-center justify-center m-20">
+      <header className="text-7xl font-bold">Voting</header>
+    </div>
+  )
+}
+
+export default IndexLogo;
