50c630c404813f2c2ce4ca9f68738bdbdc78c99d
Daniel Olczyk <44818681+MRmlik12@users.noreply.github.com>
2022-04-03 20:04:23 +0200

    Add index prototype view (#18)

---
+++ frontend/components/index/IndexContent.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+const IndexContent = () => {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <button className="border-4 border-blue-700 bg-blue-700 p-5 m-10 rounded-2xl text-4xl w-full">Create Voting</button>
+      <button className="border-4 border-blue-700 bg-blue-700 p-5 m-10 rounded-2xl text-4xl w-full">Join Voting</button>
+    </div>
+  );
+}
+
+export default IndexContent;
